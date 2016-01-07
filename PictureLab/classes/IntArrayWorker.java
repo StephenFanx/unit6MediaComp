4675f929134666a54c6412516fbb99741727595e
StephenFanx <szfan@NN123-HA-19.HS.sd203.org>
2016-01-07 15:10:23 -0600

    asdf
    zfg

--- PictureLab/classes/IntArrayWorker.java
+++ PictureLab/classes/IntArrayWorker.java
@@ -98,5 +98,24 @@ public class IntArrayWorker
       }
     }
   }
+  
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public int getCount(
+    )
+    {
+        // put your code here
+        
+    }
+
  
 }
